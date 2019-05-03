523e97e64efcca83a54b8ea1e5a6d17296a03774
Arnon Keereena <a.keereena@gmail.com>
2019-05-03 10:29:30 +0700

    - Test access token plugin and logger plugin - Enhance logger system to spew out curl request instead   make it easier to test out calls

--- Sources/RxNetworking/Plugins/AccessTokenPlugin.swift
+++ Sources/RxNetworking/Plugins/AccessTokenPlugin.swift
@@ -42,3 +42,4 @@ public class AccessTokenPlugin: PluginType {
     return tuple
   }
 }
+
--- Sources/RxNetworking/Plugins/LoggerPlugin.swift
+++ Sources/RxNetworking/Plugins/LoggerPlugin.swift
@@ -8,7 +8,7 @@ import RxSwift
 public class LoggerPlugin: PluginType {
   public func willSend(httpRequest: HTTPRequest, request: URLRequest) throws -> Single<URLRequest> {
     return Single.just(request)
-                 .do(onNext: { print($0) })
+                 .do(onNext: { print($0.description) })
   }
   
   public func didSend(request: URLRequest, received response: HTTPResponse) throws -> Single<HTTPResponse> {
@@ -16,3 +16,23 @@ public class LoggerPlugin: PluginType {
                  .do(onNext: { print($0) })
   }
 }
+
+extension URLRequest: CustomStringConvertible {
+  public var description: String {
+    let data = self.httpBody ?? Data()
+    let headers = self.allHTTPHeaderFields?.enumerated().reduce("") {
+      let base = "-H '\($1.element.key): \($1.element.value)'"
+      return $1.offset == 0 ? base : "\($0 ?? "") \\\n     \(base)"
+    } ?? ""
+    let body = String(data: data, encoding: .ascii) ?? String(data: data, encoding: .utf8) ?? ""
+    
+    return """
+           curl -X \(self.httpMethod ?? "") \\
+                -d '\(body)' \\
+                \(headers) \\
+                '\(self.url?.absoluteString ?? "")' -vs
+           
+           """
+  }
+}
+
